085015ce1ba1f44aa45229887a5277f34c917239
Matheus <mathk004@gmail.com>
2026-02-10 11:21:01 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+from auth_routes import auth_router
+from order_routes import order_router
+
+
+app.include_router(auth_router)
+app.include_router(order_router)
+# para rodar no nosso código, executar no terminal: uvicorn main:app --reload
