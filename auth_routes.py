2b4b2652b9fb590aa1835f8ba71432b355814b21
Matheus <mathk004@gmail.com>
2026-02-13 17:42:13 -0300

    feat: criando o criar conta

--- auth_routes.py
+++ auth_routes.py
@@ -1,10 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from models import Usuario
+from dependences import pegar_sessao
+from main import bycrypt_context
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
-async def autenticar():
+async def home():
     """
     Rota padrão para autenticação de usuário. Todas as rotas de pedidos precisam de autenticação.
     """
     return {'mensagem': 'autenticação de usuário', 'status': False}
+
+@auth_router.post("/criar_conta")
+async def criar_conta(email: str, senha: str, nome: str, session = Depends(pegar_sessao)):
+
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+
+    if usuario:
+        return {'mensagem': 'email já cadastrado'}
+    else:
+        senha_criptografada = bycrypt_context.hash(senha)
+        novo_usuario = Usuario(nome, email, senha_criptografada)
+        session.add(novo_usuario)
+        session.commit()
+        return {'mensagem': 'conta criada com sucesso'}
