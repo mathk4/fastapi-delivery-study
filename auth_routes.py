4be8f470c448e02a681c37edd9d150eb4c9c6b1c
Matheus <mathk004@gmail.com>
2026-02-19 15:46:42 -0300

    fazendo codigo 400 e schemas

--- auth_routes.py
+++ auth_routes.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario
 from dependences import pegar_sessao
 from main import bycrypt_context
+from schemas import UsuarioSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -13,15 +15,15 @@ async def home():
     return {'mensagem': 'autenticação de usuário', 'status': False}
 
 @auth_router.post("/criar_conta")
-async def criar_conta(email: str, senha: str, nome: str, session = Depends(pegar_sessao)):
+async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao)):
 
-    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+    usuario = session.query(Usuario).filter(Usuario.email == usuario_schema.email).first()
 
     if usuario:
-        return {'mensagem': 'email já cadastrado'}
+        raise HTTPException(status_code=400, detail="Email já cadastrado")
     else:
-        senha_criptografada = bycrypt_context.hash(senha)
-        novo_usuario = Usuario(nome, email, senha_criptografada)
+        senha_criptografada = bycrypt_context.hash(usuario_schema.senha)
+        novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada, usuario_schema.ativo, usuario_schema.admin)
         session.add(novo_usuario)
         session.commit()
-        return {'mensagem': 'conta criada com sucesso'}
+        return {'mensagem': f'conta criada com sucesso {usuario_schema.email}'}
---
+++ schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class UsuarioSchema(BaseModel):
+    nome: str
+    email: str
+    senha: str
+    ativo: Optional[bool] = True
+    admin: Optional[bool] = False
+
+    class Config:
+        from_atribute = True
