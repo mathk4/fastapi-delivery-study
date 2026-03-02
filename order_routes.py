3e8fd8d00f4b62901e27ad0eb21cdec9afd9b894
Matheus <mathk004@gmail.com>
2026-03-02 14:14:16 -0300

    feat: feito o Cancelar pedido

--- order_routes.py
+++ order_routes.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from sqlalchemy.orm import Session
 from fastapi import Depends
-from dependences import pegar_sessao
+from dependences import pegar_sessao, verificar_token
 from schemas import PedidoSchema
-from models import Pedido
+from models import Pedido, Usuario
 
-order_router = APIRouter(prefix="/orders", tags=["orders"])
+order_router = APIRouter(prefix="/orders", tags=["orders"], dependencies=[Depends(verificar_token)])
 
 @order_router.get("/")
 async def pedidos():
@@ -20,3 +20,18 @@ async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(p
     session.add(novo_pedido)
     session.commit()
     return {'mensagem': f'pedido criado com sucesso! ID do pedido: {novo_pedido.id_pedido}'}
+
+@order_router.post("/pedido/cancelar/{id_pedido}")
+async def cancelar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao), usuario: Usuario = Depends(verificar_token)):
+    pedido = session.query(Pedido).filter(Pedido.id_pedido == id_pedido).first()
+    if not pedido:
+        raise HTTPException(status_code=400, detail="Pedido não encontrado")
+    if not usuario.admin or usuario.id_usuario != pedido.usuario:
+        raise HTTPException(status_code=401, detail="Autorização negada")
+
+    pedido.status = "CANCELADO"
+    session.commit()
+    return {
+        'mensagem': f'pedido {pedido.id_pedido} cancelado com sucesso!',
+        'pedido': pedido
+        }
